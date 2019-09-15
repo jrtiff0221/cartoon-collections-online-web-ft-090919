82057cc7d140434fefa7532286771e324d2db7f5
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-15 03:14:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(names)
-  return names.collect { |word| word.upcase }.join(" ! ")
+  return names.collect { |names| names.upcase }.join(" ! ")
 end
 
 def long_planeteer_calls# code an argument here
